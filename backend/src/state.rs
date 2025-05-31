266817706bfea5ddb140ca33afb0d7e3a8e31524
akagiyuu <khanghuynh0245@gmail.com>
2025-05-31 13:22:05 +0700

    feat(state): add google oidc client

--- backend/src/state.rs
+++ backend/src/state.rs
@@ -2,16 +2,38 @@ use std::sync::Arc;
 
 use sqlx::PgPool;
 
-use crate::config::CONFIG;
+use crate::{config::CONFIG, util};
+use openidconnect::reqwest;
 
 pub struct ApiState {
     pub database_pool: PgPool,
+    pub http_client: reqwest::Client,
+    pub google_client: util::auth::Client,
 }
 
 impl ApiState {
     pub async fn new() -> Arc<Self> {
         let database_pool = PgPool::connect(&CONFIG.database_url).await.unwrap();
 
-        Arc::new(Self { database_pool })
+        let http_client = reqwest::ClientBuilder::new()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap();
+
+        let google_client = util::auth::new(
+            CONFIG.google_client_id.clone(),
+            CONFIG.google_client_secret.clone(),
+            CONFIG.google_issuer_url.clone(),
+            CONFIG.google_redirect_url.clone(),
+            &http_client,
+        )
+        .await
+        .unwrap();
+
+        Arc::new(Self {
+            database_pool,
+            http_client,
+            google_client,
+        })
     }
 }
