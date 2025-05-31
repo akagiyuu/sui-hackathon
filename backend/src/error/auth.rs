57e13b586404e31f7752f37ddc22868a1c6b4698
akagiyuu <khanghuynh0245@gmail.com>
2025-05-31 21:34:40 +0700

    refactor: remove unused error type

--- backend/src/error/auth.rs
+++ backend/src/error/auth.rs
@@ -4,34 +4,18 @@ use super::ErrorResponse;
 
 #[derive(Debug, thiserror::Error)]
 pub enum AuthError {
-    #[error("Account with given email already existed")]
-    AccountExisted,
-
-    #[error("Invalid login data")]
-    InvalidLoginData,
-
     #[error("Missing authentication token")]
     MissingAuthToken,
 
     #[error("Invalid authentication token")]
     InvalidAuthToken,
-
-    #[error("Missing required permission")]
-    MissingPermission,
-
-    #[error("Account is not activated")]
-    ActivationRequired,
 }
 
 impl IntoResponse for AuthError {
     fn into_response(self) -> axum::response::Response {
         let status = match self {
-            AuthError::AccountExisted => StatusCode::BAD_REQUEST,
-            AuthError::InvalidLoginData => StatusCode::UNAUTHORIZED,
             AuthError::MissingAuthToken => StatusCode::UNAUTHORIZED,
             AuthError::InvalidAuthToken => StatusCode::UNAUTHORIZED,
-            AuthError::MissingPermission => StatusCode::FORBIDDEN,
-            AuthError::ActivationRequired => StatusCode::FORBIDDEN,
         };
 
         let response = ErrorResponse {
