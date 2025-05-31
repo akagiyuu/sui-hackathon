c25daeac42b89774eedef0cd3b9665d73e1ce7dc
akagiyuu <khanghuynh0245@gmail.com>
2025-05-31 11:33:28 +0700

    feat: add watch page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 import { Home } from './pages/home';
 import { MainLayout } from './layout';
 import { UploadForm } from './pages/upload';
+import Watch from './pages/watch';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Route element={<MainLayout />}>
                 <Route index element={<Home />} />
                 <Route path="/upload" element={<UploadForm />} />
+                <Route path="/watch/:id" element={<Watch />} />
             </Route>
         </Routes>
     );
