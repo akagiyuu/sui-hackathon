67e787199884116b23cd20089ebf6eb647eeba3a
akagiyuu <khanghuynh0245@gmail.com>
2025-05-31 10:02:35 +0700

    feat(home): create mock video list

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,9 +1,50 @@
-import { Button } from '@/components/ui/button';
+import { VideoCard, type VideoCardProps } from '@/components/video-card';
+
+const videos: VideoCardProps[] = [
+    {
+        animationDelay: 100,
+        thumbnail: '/placeholder.svg',
+        title: 'Building a Modern Web App with Next.js',
+        uploader: 'Tech Tutorials',
+        duration: 600,
+        view: 100,
+        createAt: new Date(Date.now()),
+    },
+    {
+        animationDelay: 200,
+        thumbnail: '/placeholder.svg',
+        title: 'Building a Modern Web App with Next.js',
+        uploader: 'Tech Tutorials',
+        duration: 600,
+        view: 100,
+        createAt: new Date(Date.now()),
+    },
+    {
+        animationDelay: 300,
+        thumbnail: '/placeholder.svg',
+        title: 'Building a Modern Web App with Next.js',
+        uploader: 'Tech Tutorials',
+        duration: 600,
+        view: 100,
+        createAt: new Date(Date.now()),
+    },
+    {
+        animationDelay: 400,
+        thumbnail: '/placeholder.svg',
+        title: 'Building a Modern Web App with Next.js',
+        uploader: 'Tech Tutorials',
+        duration: 600,
+        view: 100,
+        createAt: new Date(Date.now()),
+    },
+];
 
 export function Home() {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button>Click me</Button>
+        <div className="mx-auto p-7 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {videos.map((video) => (
+                <VideoCard {...video} />
+            ))}
         </div>
     );
 }
