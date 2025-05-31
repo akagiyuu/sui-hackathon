4b3236bd7132671b90b46d7d02656ed3bf3ad605
akagiyuu <khanghuynh0245@gmail.com>
2025-05-31 17:21:30 +0700

    fix(video): parse into f32 first and round

--- backend/src/util/video.rs
+++ backend/src/util/video.rs
@@ -14,10 +14,15 @@ const ARGS: &[&str] = &[
 ];
 
 pub async fn get_duration(path: impl AsRef<Path>) -> Result<i32> {
-    let child = Command::new(BINARY).args(ARGS).arg(path.as_ref()).spawn()?;
+    let output = Command::new(BINARY)
+        .args(ARGS)
+        .arg(path.as_ref())
+        .output()
+        .await?;
 
-    let output = child.wait_with_output().await?.stdout;
-    let duration = String::from_utf8(output)?.parse()?;
+    let stdout = String::from_utf8(output.stdout)?;
 
-    Ok(duration)
+    let duration = stdout.trim().parse::<f32>()?;
+
+    Ok(duration.round() as i32)
 }
