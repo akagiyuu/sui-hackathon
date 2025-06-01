cc4046ee1c85a86fa603cc4c547b6ad80990dc38
akagiyuu <khanghuynh0245@gmail.com>
2025-06-01 10:40:47 +0700

    feat: add search button

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 import { Logo } from './logo';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Bell, Search, Upload } from 'lucide-react';
+import { ArrowRight, Bell, Search, Upload } from 'lucide-react';
 import { Account } from './account';
 import { useState } from 'react';
 
@@ -40,6 +40,17 @@ export function Header() {
                                         aria-hidden="true"
                                     />
                                 </div>
+                                <button
+                                    className="absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 outline-offset-2 transition-colors hover:text-foreground focus:z-10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+                                    aria-label="Submit search"
+                                    onClick={onSearch}
+                                >
+                                    <ArrowRight
+                                        size={16}
+                                        strokeWidth={2}
+                                        aria-hidden="true"
+                                    />
+                                </button>
                             </div>
                         </div>
                     </div>
